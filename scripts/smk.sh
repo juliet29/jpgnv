94d9a76d8c02b0ea07f73a094b26290dec4fdd7b
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2026-03-11 10:32:33 -0700

    add smk script

---
+++ scripts/smk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SMKCMD="uv run snakemake -s ../workflow/Snakefile"
+
+# run-qoi:
+# uv run snakemake -c 1 qoi_create_target
+# uv run snakemake -c 1 qoi_consolidate_target
+#
+# run-jpg:
+# uv run snakemake -c 1 jpg_create_target
+# uv run snakemake -c 1 jpg_consolidate_target
